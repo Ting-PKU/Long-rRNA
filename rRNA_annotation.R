6bbe01f2d7d3d5276096e7c1426cf305ca35562e
Ting Peng <70944124+Ting-PKU@users.noreply.github.com>
2024-07-14 18:23:50 +0800

    Update rRNA_annotation.R

--- rRNA_annotation.R
+++ rRNA_annotation.R
@@ -5,5 +5,5 @@ colnames(df) <- c('read_count','long_anno')
 meta <- readRDS(paste0(out,'annotation_metadata.rds'))
 df <- merge(df,meta,by = 'long_anno')
 df <- df[order(df$read_count,decreasing = T),]
-write.table(df,paste0(out,'rRNA_count.tsv'),
+write.table(df[,c(1,4,3,2],paste0(out,'rRNA_count.tsv'),
             row.names = F,quote = F,sep = '\t')
