3cf7a9cb1861f4f61191b03f949be27d8c5e19d2
Ting Peng <70944124+Ting-PKU@users.noreply.github.com>
2024-07-14 17:10:57 +0800

    Add files via upload

---
+++ rRNA_annotation.R
@@ -0,0 +1,7 @@
+df <- read.table(paste0(out,'annotation_count'))
+colnames(df) <- c('read_count','long_anno')
+meta <- readRDS(paste0(out,'annotation_metadata.rds'))
+df <- merge(df,meta,by = 'long_anno')
+df <- df[order(df$read_count,decreasing = T),]
+write.table(df,paste0(out,'rRNA_count.tsv'),
+            row.names = F,quote = F,sep = '\t')
